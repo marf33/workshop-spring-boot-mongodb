0d67cde2dbd97de6ec32ab0f8eab6d503b8cd5fc
Miguel Figueiredo <miguel.figueiredo96@gmail.com>
2024-03-07 12:08:55 +0000

    Obter post por id

---
+++ src/main/java/com/miguelfigueiredo/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.miguelfigueiredo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miguelfigueiredo.workshopmongo.domain.Post;
+import com.miguelfigueiredo.workshopmongo.domain.User;
+import com.miguelfigueiredo.workshopmongo.repository.PostRepository;
+import com.miguelfigueiredo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+}
