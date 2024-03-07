3b69bce63ac93cffe82341922c1745d8e75f930a
Miguel Figueiredo <miguel.figueiredo96@gmail.com>
2024-03-07 10:22:43 +0000

    Apagar utilizador com DELETE

--- src/main/java/com/miguelfigueiredo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/miguelfigueiredo/workshopmongo/resources/UserResource.java
@@ -51,4 +51,11 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();		
 	}
+	
+	//Apagar utilizador
+	@RequestMapping(method =  RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/miguelfigueiredo/workshopmongo/services/UserService.java
+++ src/main/java/com/miguelfigueiredo/workshopmongo/services/UserService.java
@@ -10,30 +10,33 @@ import com.miguelfigueiredo.workshopmongo.domain.User;
 import com.miguelfigueiredo.workshopmongo.dto.UserDTO;
 import com.miguelfigueiredo.workshopmongo.repository.UserRepository;
 import com.miguelfigueiredo.workshopmongo.services.exception.ObjectNotFoundException;
-import com.mongodb.client.model.ReturnDocument;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
-	
-	
+
 }
